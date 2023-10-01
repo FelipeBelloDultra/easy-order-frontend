eaf4809c1a79cfcd56c67f85fbda5369a135d45b
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2023-10-01 11:54:15 -0300

    fix(components): rename components to kabeb-case

--- src/components/core/Input/InputError.tsx
+++
@@ -1,9 +0,0 @@
-import { ContainerError } from "./styles";
-
-type InputErrorProps = {
-  massage: string;
-};
-
-export function InputError({ massage }: InputErrorProps) {
-  return <ContainerError>{massage}</ContainerError>;
-}
